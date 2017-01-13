3b319321ecfd37abea5be996fbd75febda289131
curtknutson <curtknutson@me.com>
2017-01-13 12:59:10 -0600

    initial commit item 46

---
+++ console_pythonItem46.py
@@ -0,0 +1,21 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 22 2016, 17:23:13) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: /Users/curts/Documents/Curt/School/TechAcademy/Python/script_pythonItem46.py 
+range with one parameter
+0
+1
+2
+3
+range with three parameters
+3
+2
+1
+0
+range with three parameters
+8
+6
+4
+2
+>>> 
---
+++ script_pythonItem46.py
@@ -0,0 +1,24 @@
+# Python: 3.6.0
+
+# Author: Curt Knutson
+
+#Purpose: Learning Python Tech Academy
+
+#Item 46
+
+def start():
+    print("range with one parameter")
+    for i in range(4):
+        print(i)
+    print("range with three parameters")
+    for i in range(3,-1,-1):
+        print(i)
+    print("range with three parameters")
+    for i in range(8,0,-2):
+        print(i)
+
+
+
+
+if __name__ == "__main__":
+    start()
